647db4850a1c355b742ce82b134f3f9ecc6b0e94
Connor <50525187+WobblyRonnoc@users.noreply.github.com>
2024-01-15 16:27:36 -0500

    progress on analog display

--- analog_input_viewer.gd
+++ analog_input_viewer.gd
@@ -1,21 +1,46 @@
 extends Node2D
 
-var radius : float = 31
+#TODO: lock to cardinal until at edge (no changing mind before done reaching for edge)
+
+@export var radius : float = 31
+@onready var id_coords : Dictionary = {"up": Vector2(0, -radius), "right": Vector2(radius, 0), "Down": Vector2(0, radius), "left": Vector2(-radius, 0)}
+
 @onready var analog = $AnalogPosition
 @onready var back = $back
+@onready var analog_vector
 
 func _process(delta):
-	var analog_vector = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-	analog.position = analog_vector * radius
-	analog.position.clamp(position, analog_vector * radius)
-	
 	Global.analog_position = analog.position
 	Global.analog_angle = rad_to_deg(back.position.angle_to(analog.position))
 	
-	# FIXME: this is gross
-	if (Global.analog_angle > -45.0 && Global.analog_angle < 45.0) || (Global.analog_angle > -145.0 && Global.analog_angle < 360.0 || Global.analog_angle < 145.0 && Global.analog_angle < 180.0 ):
-		analog.position.y = 0
+	analog_vector = Input.get_vector("ui_left","ui_right","ui_up","ui_down",-1.0)
+	analog.position = analog_vector * radius
+	analog.position.clamp(position, analog_vector * radius)
 	
+	if analog.position.length() < 28:
+		lock_to_cardinals()
+	else:
+		analog.scale.x = 20
+		analog.scale.y = 20
+		
+func lock_to_cardinals():
+	analog.scale.x = 10
+	analog.scale.y = 10
+
+	# lock the position to cardinals until at the outer circumference
+
+	#up
 	if (Global.analog_angle > -145.0 && Global.analog_angle < -45.0):
 		analog.position.x = 0
-	
+
+	#down
+	if (Global.analog_angle < 145.0 && Global.analog_angle > 45.0):
+		analog.position.x = 0
+
+	#right
+	if (Global.analog_angle > -45.0 && Global.analog_angle < 45.0):
+		analog.position.y = 0
+
+	#left
+	if ( (Global.analog_angle < -145.0 && Global.analog_angle > -179.0) || (Global.analog_angle > 145.0 && Global.analog_angle < 180.0) ):
+		analog.position.y = 0
